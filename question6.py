c280cfd48f70b5f1d4cff1118726703a99d50fb4
baudoin <baudoinvicbolingo@gmail.com>
2024-02-09 00:12:19 +0200

    adding page 6

---
+++ question6.py
@@ -0,0 +1,38 @@
+import random
+
+data = [['0', '0'], ['1', '1'], ['u', 'v']]
+
+level = 1
+
+def start_message():
+    print('Input cell number (e.g. A1) of different character')
+
+def section_message():
+    print('level:' + str(level))
+
+def view_question():
+    choice_data = random.randint(0, 2)
+    mistake_number = random.randint(0, 8)
+    print('Debug: mistake_number = ' + str(mistake_number))
+    question = data[choice_data]
+    i = 0
+    while i < 3:
+        question_str = ''
+        j = 0
+        while j < 3:
+            if (i * 3 + j) == mistake_number:
+                question_str += question[1]
+            else:
+                question_str += question[0]
+            j += 1
+        print(question_str)
+        i += 1
+
+def play():
+    section_message()
+    view_question()
+    choice = input('(e.g. A1)')
+    print('Debug: choice = ' + choice)
+
+start_message()
+play()
